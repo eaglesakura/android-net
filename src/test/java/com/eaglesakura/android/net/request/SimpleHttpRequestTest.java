087f528cd89f66c655ec996129f86d4998fca3bc
eaglesakura <eagle.sakura@gmail.com>
2016-03-09 18:05:28 +0900

    HTTP URL組み立てのテストを追加

---
+++ src/test/java/com/eaglesakura/android/net/request/SimpleHttpRequestTest.java
@@ -0,0 +1,56 @@
+package com.eaglesakura.android.net.request;
+
+import com.eaglesakura.android.net.CiJUnitTester;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+public class SimpleHttpRequestTest extends CiJUnitTester {
+
+    @Test
+    public void get用URLを構築する() throws Exception {
+        SimpleHttpRequest req = new SimpleHttpRequest(ConnectRequest.Method.GET);
+
+
+        {
+            req.setUrl("https://example.com", null);
+            assertEquals(req.getUrl(), "https://example.com");
+        }
+
+        {
+            Map<String, String> values = new HashMap<>();
+            values.put("key", "value");
+            req.setUrl("https://example.com", values);
+            assertEquals(req.getUrl(), "https://example.com?key=value&");
+        }
+
+        {
+            Map<String, String> values = new HashMap<>();
+            values.put("key", "value");
+            req.setUrl("https://example.com?def=value", values);
+            assertEquals(req.getUrl(), "https://example.com?def=value&key=value&");
+        }
+    }
+
+    @Test
+    public void post用URLを構築する() throws Exception {
+        SimpleHttpRequest req = new SimpleHttpRequest(ConnectRequest.Method.POST);
+
+        {
+            req.setUrl("https://example.com", null);
+            assertEquals(req.getUrl(), "https://example.com");
+        }
+
+        {
+            Map<String, String> values = new HashMap<>();
+            values.put("key", "value");
+            req.setUrl("https://example.com", values);
+            assertEquals(req.getUrl(), "https://example.com");
+        }
+    }
+
+}
