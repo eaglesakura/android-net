e158feca3f7a0fc2d1cff05f5e5c92026daecafc
eaglesakura <eagle.sakura@gmail.com>
2016-04-04 19:39:14 +0900

    ローカルファイルへのDLを行うParserを追加

--- src/main/java/com/eaglesakura/android/net/parser/RequestParser.java
+++ src/main/java/com/eaglesakura/android/net/parser/RequestParser.java
@@ -9,6 +9,8 @@ import java.io.InputStream;
  * オブジェクトのパースを行う
  * <p>
  * InputStream内でキャンセルチェックを行い、必要に応じて例外を投げるため明示的なcancelチェックを行わない。
+ *
+ * 入力されたdataのclose処理は外部で行うため、内部で気にする必要はない。
  */
 public interface RequestParser<T> {
     T parse(Result<T> sender, InputStream data) throws Exception;
